36b8ad2481a1d70a18449b8b9050722128080a13
Delforojas <141573441+Delforojas@users.noreply.github.com>
2023-08-06 18:21:12 +0200

    Calculo de coste de viaje.
    Programa para realizar el coste de un viaje , introduciendo kms, precio de gasolina y consumo del coche

---
+++ costeviaje1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace costtravel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("vamos a calcular el coste de gasolina del viaje;");
+            Console.WriteLine("dime cuantos kms vas a recorrer?");
+            double kilometrosRecorridos = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce el precio de la gasolina");
+            double precioGasolina = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce el consumo de tu coche");
+            double precioconsumo = Convert.ToDouble(Console.ReadLine());
+
+
+            double kilometrosPasados = kilometrosRecorridos / 100;
+            double consumoviaje = kilometrosRecorridos * kilometrosPasados;
+            double precioviaje = consumoviaje / precioGasolina; 
+
+            Console.WriteLine($"el viaje de {kilometrosRecorridos} kilometros te costara {precioviaje}");
+
+
+
+        }
+    }
+}
