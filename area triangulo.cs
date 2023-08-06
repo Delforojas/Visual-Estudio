38b34834c0d773f180a1b00e0d220db052850934
Delforojas <141573441+Delforojas@users.noreply.github.com>
2023-08-06 18:07:41 +0200

    Area de triangulo
    Programa para calcular el área de un triangulo.

---
+++ area triangulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace area_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String frase = Console.ReadLine();
+            Console.WriteLine(frase.Replace(" ", "").ToLower());
+
+
+            int letra = 0
+            int consonantes = Convert.Int32Parse(frase)
+            int f = 0;
+
+            letra = frase[f];
+            for (f = 0; f < frase.Length; f++)
+            {
+
+
+                if (( letra == "a") | (letra == "e") | (letra == "i") | (letra == "o") | (letra == "u"))
+                {
+                    letra++;
+                }
+                else if ((letra == "b") | (letra == "c") | (letra == "d") | (letra == "f") | (letra == "g") | (letra == "h") | (letra == "j") | (letra == "k") | (letra == "l") | (letra == "m") | (letra == "n") | (letra == "ñ") | (letra == "p") |) (letra == "q") | (letra == "r") | (letra == "s") | (letra == "t") | (letra == "v") | (letra == "w") | (letra == "x") | (letra == "y") | (letra == "z"))
+                {
+                consonantes++;
+                }
+            }
+        Console.;
+        }    
+
+            
+
+
+
+
+
+
+
+
+        
+
+    }
+}
