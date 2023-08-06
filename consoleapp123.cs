7c5f227393903b417a64dfd4ce8c98c95eabb4ef
Delforojas <141573441+Delforojas@users.noreply.github.com>
2023-08-06 18:17:53 +0200

    Conversor de euros a pesetas,
    Programa para realizar la conversión de euros a pesetas.

---
+++ consoleapp123.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca la cantidad de euros:");
+            const euros = Convert.ToDouble(Console.ReadLine());
+            double pesetas = euros * 166;
+            Console.WriteLine("{0} euros son {1}", euros, pesetas);
+            Console.WriteLine($"{euros} euros equivale a {pesetas} pesetas.");
+        }
+    }
+}
